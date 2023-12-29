816bc308e0b3ddfed8d480fb4ef602f6296bd490
felix mavila <felixmacarenco@gmail.com>
2023-12-29 05:28:11 +0000

    config: preparing app to production environment

--- dist/shared/container/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-var _UserRepository = require("../../modules/Users/infra/repository/UserRepository");
-_tsyringe.container.registerSingleton('UserRepository', _UserRepository.UserRepository);
