b2062663e0922c96f3d91f8729add53b74acf5ea
felix mavila <felixmacarenco@gmail.com>
2023-12-31 20:50:46 +0000

    feat: create password hash

--- src/modules/Users/useCases/registerUser/registerUserUseCases.ts
+++ src/modules/Users/useCases/registerUser/registerUserUseCases.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata'
 import { inject, injectable } from "tsyringe"
 import { IUserDTO } from "../../infra/entities/User"
 import { IUserRepository } from "../../infra/repository/IUserRepository"
-import { hash } from 'bcryptjs'
+import * as argon2 from 'argon2';
 
 @injectable()
 class RegisterUserUseCases{
@@ -15,7 +15,7 @@ class RegisterUserUseCases{
 
    
     async execute({email,name,password,phone}:IUserDTO):Promise<void>{
-      const PasswordHash = await hash(password,8);
+      const PasswordHash = await argon2.hash(password);
       await this.userRepository.register({email,name,password:PasswordHash,phone})
     }
 
